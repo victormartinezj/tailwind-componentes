fd8b626455cfc06aabfeff123188093605fe514b
victor <udemyreactfull@gmail.com>
2021-06-13 01:06:26 -0500

    3.iii.popover

--- index.js
+++ index.js
@@ -21,3 +21,37 @@ tippy("#tooltip-t-html", {
   content:
     "<span class='text-red-500 font-semibold'>Tooltip</span> <span class='italic text-green-500'>with </span><span class='underline text-blue-500'>Tailwind</span> 🤫 ",
 });
+
+// popover
+
+tippy("#popover-t", {
+  theme: "light-border",
+  content: "Top popover",
+  trigger: "click",
+  hideOnClick: "toggle",
+  interactive: true,
+});
+tippy("#popover-b", {
+  theme: "light-border",
+  placement: "bottom",
+  content: "Bottom popover",
+  trigger: "click",
+  hideOnClick: "toggle",
+  interactive: true,
+});
+tippy("#popover-r", {
+  theme: "light-border",
+  placement: "right",
+  content: "Right popover",
+  trigger: "click",
+  hideOnClick: "toggle",
+  interactive: true,
+});
+tippy("#popover-l", {
+  theme: "light-border",
+  placement: "left",
+  content: "Left popover",
+  trigger: "click",
+  hideOnClick: "toggle",
+  interactive: true,
+});
