c8dfb5e546272eb671325ec51e43602009a9b800
victor <udemyreactfull@gmail.com>
2021-06-13 00:01:21 -0500

    3.ii.tooltip-html

--- index.js
+++ index.js
@@ -13,3 +13,11 @@ tippy("#tooltip-l", {
   content: "Tooltip on bottom",
   placement: "left",
 });
+
+// html
+
+tippy("#tooltip-t-html", {
+  allowHTML: true,
+  content:
+    "<span class='text-red-500 font-semibold'>Tooltip</span> <span class='italic text-green-500'>with </span><span class='underline text-blue-500'>Tailwind</span> 🤫 ",
+});
