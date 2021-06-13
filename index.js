b41cb69e6b644f31345204059602ef6b49585afd
victor <udemyreactfull@gmail.com>
2021-06-12 21:49:39 -0500

    3.i.tooltip-base

---
+++ index.js
@@ -0,0 +1,15 @@
+tippy("#tooltip-t", {
+  content: "Tooltip on top",
+});
+tippy("#tooltip-b", {
+  content: "Tooltip on bottom",
+  placement: "bottom",
+});
+tippy("#tooltip-r", {
+  content: "Tooltip on bottom",
+  placement: "right",
+});
+tippy("#tooltip-l", {
+  content: "Tooltip on bottom",
+  placement: "left",
+});
